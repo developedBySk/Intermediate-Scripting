ac61e66dd91f9f041d15ce1f8e0301bb7aedafd4
Shatmanaya <sanjaykmr157@outlook.com>
2021-02-19 12:15:48 +0530

    sortedTheUnions

--- sortedUnion.js
+++ sortedUnion.js
@@ -1,10 +1,19 @@
 function uniteUnique(arr) {
     var newArray=[];
+    var finalArray=[];
     var args = Array.prototype.slice.call(arguments)
-    for(var i=0;i<args.length;i++){
+    for(let i=0;i<args.length;i++){
         args[i].map(item => newArray.push(item))
     }
-    return newArray;
+    for(let j =0;j<newArray.length;j++){
+        for(let k=0;k<newArray.length;k++){
+            var indexValue = newArray[k];
+            if(finalArray.indexOf(indexValue)==-1){
+                finalArray.push(indexValue);
+            }
+        }
+    }
+    return finalArray;
 }
   
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
