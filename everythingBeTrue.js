2fd201299b389ccacfe576cb9f7027f2f24d630c
Shatmanaya <sanjaykmr157@outlook.com>
2021-03-03 12:32:59 +0530

    everthingBetrueSolved

--- everythingBeTrue.js
+++ everythingBeTrue.js
@@ -8,3 +8,14 @@ In JavaScript, truthy values are values that translate to true when evaluated in
 
 Remember, you can access object properties through either dot notation or [] notation.
 */
+
+function truthcheck(collection,pre){
+    let counter=0
+    for(var a in collection){
+        if(collection[a].hasOwnProperty(pre) && Boolean(collection[a][pre])){
+            counter++
+        }
+    }
+    return counter == collection.length
+}
+console.log(truthcheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"))
