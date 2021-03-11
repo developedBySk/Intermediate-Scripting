6c4f5099db1ece50aa9172dc3315a77efd02bf69
Shatmanaya <sanjaykmr157@outlook.com>
2021-03-11 18:43:02 +0530

    ProjectTwoInitiated

--- ProjectPalindrome.js
+++ ProjectPalindrome.js
@@ -1,11 +1,11 @@
 function palindrome(str){
-    for(let i=0;i<=str.length/2;i++){
-      if(str[i]==str[str.length-1-i]){
-        return true;
-      }
-      else{
+    str = str.toLowerCase().replace(/[\W_]/g, "");
+     for (var i = 0, len = str.length - 1; i < len / 2; i++) {
+      if (str[i] !== str[len - i]) {
         return false;
       }
+    }
+    return true;
   }
-}
-console.log(palindrome("eye"));
+  palindrome("eye");
+console.log(palindrome("_eye"));
---
+++ ProjectRoman.js
@@ -0,0 +1,7 @@
+function convertToRoman(num) {
+    const len = num.toString()
+    let a = Math.floor(num/10)
+    console.log(a)
+}
+   
+console.log(convertToRoman(36));
