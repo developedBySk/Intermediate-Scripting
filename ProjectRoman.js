995e68ecba847addbab19f53f75f6dc6bbcd5db3
Shatmanaya <sanjaykmr157@outlook.com>
2021-03-12 12:30:38 +0530

    ProjectRomanize

--- ProjectRoman.js
+++ ProjectRoman.js
@@ -1,7 +1,30 @@
 function convertToRoman(num) {
-    const len = num.toString()
-    let a = Math.floor(num/10)
-    console.log(a)
+    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanValues = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I"
+    ];
+     var romanized = "";
+
+    for (var index = 0; index < decimalValue.length; index++) {
+        while (decimalValue[index] <= num) {
+        romanized += romanValues[index];
+        num -= decimalValue[index];
+        }
+    }
+
+  return romanized;
 }
    
-console.log(convertToRoman(36));
+console.log(convertToRoman(543));
